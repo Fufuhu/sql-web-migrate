a2bd1bb01e52637e82bcb62623ff417e4f6977b0
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2019-10-21 19:17:33 +0900

    upとdownを処理できるようにした&処理の実態を共通化

--- src/github.com/fufuhu/sql-web-migrate/main.go
+++ src/github.com/fufuhu/sql-web-migrate/main.go
@@ -15,7 +15,8 @@ import (
 func main() {
 
 	// URLパスと関数の関係を定義
-	http.HandleFunc("/migrate", execMigrate)
+	http.HandleFunc("/migrate/up", execMigrateUp)
+	http.HandleFunc("/migrate/down", execMigrateDown)
 
 	// ListenするIPアドレスを定義
 	err := http.ListenAndServe("0.0.0.0:18080", nil)
@@ -26,18 +27,50 @@ func main() {
 
 }
 
+// LogRecord DDLの適用記録を格納するための構造体
+// ID SQLのID
+// AppliedAt SQLの適用タイムスタンプ
 type LogRecord struct {
 	ID        string `json:id`
 	AppliedAt string `json:appliedAt`
 }
 
-func execMigrate(w http.ResponseWriter, r *http.Request) {
+// 一旦Postgre固定
+func getConnection(connectionConfig config.DBConnectionConfig, dialect string) (*sql.DB, error) {
 
-	addresses := r.Header.Get("X-Forwarded-For")
-	fmt.Println(addresses)
+	host := connectionConfig.Host()
+	port, err := connectionConfig.Port()
+	if err != nil {
+		fmt.Println(err)
+	}
+	sslMode, err := connectionConfig.SSLMode()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	remote := r.RemoteAddr
-	fmt.Println(remote)
+	var connectionString string
+	if host[:1] == "/" {
+		connectionString = config.BuildConnectionStringForUnixDomainSocket(
+			host,
+			connectionConfig.User(),
+			connectionConfig.Password(),
+			connectionConfig.DBName())
+	} else {
+		connectionString = config.BuildConnectionString(
+			host,
+			port,
+			connectionConfig.User(),
+			connectionConfig.Password(),
+			connectionConfig.DBName(),
+			sslMode)
+	}
+
+	db, err := sql.Open("postgres", connectionString)
+
+	return db, err
+}
+
+func execMigrate(direction migrate.MigrationDirection) ([]LogRecord, error) {
 
 	sourcePath := config.GetMigrationSourcePath()
 	fmt.Println(sourcePath)
@@ -46,40 +79,20 @@ func execMigrate(w http.ResponseWriter, r *http.Request) {
 		Dir: sourcePath,
 	}
 
-	// port, _ := config.GetPort()
-	// sslMode, _ := config.GetSSLMode()
-
-	//connectionString := "host=127.0.0.1 port=5432 user=migrate password=migrate dbname=migrate sslmode=disable"
-	// connectionString := config.BuildConnectionString(
-	// 	config.GetHost(),
-	// 	port,
-	// 	config.GetUser(),
-	// 	config.GetPassword(),
-	// 	config.GetDBName(),
-	// 	sslMode)
-
-	//https://godoc.org/github.com/lib/pq
-
-	// connectionString := "postgres://migrate:migrate@migrate/?host=/var/run/postgresql"
-	// connectionString := "host=/var/run/postgresql user=migrate password=migrate dbname=migrate"
-	connectionString := config.BuildConnectionStringForUnixDomainSocket(
-		config.GetHost(),
-		config.GetUser(),
-		config.GetPassword(),
-		config.GetDBName())
-
-	db, err := sql.Open("postgres", connectionString)
+	db, err := getConnection(
+		config.ConnectionConfig,
+		"postgres")
 
 	if err != nil {
 		fmt.Println("DB connection open failure.")
 		fmt.Println(err)
 	}
 
-	n, err := migrate.ExecMax(db, "postgres", source, migrate.Up, 0)
-
+	n, err := migrate.ExecMax(db, "postgres", source, direction, 0)
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Printf("Applied %d migrations!\n", n)
 
 	// クエリを投げる
 
@@ -97,11 +110,12 @@ func execMigrate(w http.ResponseWriter, r *http.Request) {
 
 	rows, err := statement.Query()
 
+	// 結果の取得と構造体への格納
 	records := []LogRecord{}
 	for rows.Next() {
 		err := rows.Scan(&id, &appliedAt)
 		if err != nil {
-			panic(err)
+			fmt.Println(err)
 		}
 		fmt.Println(id, appliedAt)
 		record := LogRecord{
@@ -110,9 +124,79 @@ func execMigrate(w http.ResponseWriter, r *http.Request) {
 		}
 
 		records = append(records, record)
+	}
 
-		// fmt.Fprintf(w, "%s %s\n", id, appliedAt)
+	return records, err
+}
 
+func execMigrateUp(w http.ResponseWriter, r *http.Request) {
+
+	addresses := r.Header.Get("X-Forwarded-For")
+	fmt.Println(addresses)
+
+	remote := r.RemoteAddr
+	fmt.Println(remote)
+
+	//sourcePath := config.GetMigrationSourcePath()
+	//fmt.Println(sourcePath)
+
+	//source := migrate.FileMigrationSource{
+	//	Dir: sourcePath,
+	//}
+
+	//db, err := getConnection(
+	//	config.ConnectionConfig,
+	//	"postgres")
+
+	//if err != nil {
+	//	fmt.Println("DB connection open failure.")
+	//	fmt.Println(err)
+	//}
+
+	//n, err := migrate.ExecMax(db, "postgres", source, migrate.Up, 0)
+
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+
+	//// クエリを投げる
+
+	//statement, err := db.Prepare("select id, applied_at from gorp_migrations")
+
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//defer statement.Close()
+
+	//var (
+	//	id        string
+	//	appliedAt time.Time
+	//)
+
+	//rows, err := statement.Query()
+
+	//// 結果の取得と構造体への格納
+	//records := []LogRecord{}
+	//for rows.Next() {
+	//	err := rows.Scan(&id, &appliedAt)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	fmt.Println(id, appliedAt)
+	//	record := LogRecord{
+	//		ID:        id,
+	//		AppliedAt: appliedAt.String(),
+	//	}
+
+	//	records = append(records, record)
+
+	//	// fmt.Fprintf(w, "%s %s\n", id, appliedAt)
+
+	//}
+
+	records, err := execMigrate(migrate.Up)
+	if err != nil {
+		fmt.Println(err)
 	}
 
 	bytes, _ := json.Marshal(records)
@@ -121,5 +205,83 @@ func execMigrate(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "%s\n", recordString)
 
-	fmt.Printf("Applied %d migrations!\n", n)
+	// fmt.Printf("Applied %d migrations!\n", n)
+}
+
+func execMigrateDown(w http.ResponseWriter, r *http.Request) {
+
+	addresses := r.Header.Get("X-Forwarded-For")
+	fmt.Println(addresses)
+
+	remote := r.RemoteAddr
+	fmt.Println(remote)
+
+	// sourcePath := config.GetMigrationSourcePath()
+	// fmt.Println(sourcePath)
+
+	// source := migrate.FileMigrationSource{
+	// 	Dir: sourcePath,
+	// }
+
+	// db, err := getConnection(
+	// 	config.ConnectionConfig,
+	// 	"postgres")
+
+	// if err != nil {
+	// 	fmt.Println("DB connection open failure.")
+	// 	fmt.Println(err)
+	// }
+
+	// n, err := migrate.ExecMax(db, "postgres", source, migrate.Down, 0)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// // クエリを投げる
+
+	// statement, err := db.Prepare("select id, applied_at from gorp_migrations")
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// defer statement.Close()
+
+	// var (
+	// 	id        string
+	// 	appliedAt time.Time
+	// )
+
+	// rows, err := statement.Query()
+
+	// records := []LogRecord{}
+	// for rows.Next() {
+	// 	err := rows.Scan(&id, &appliedAt)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Println(id, appliedAt)
+	// 	record := LogRecord{
+	// 		ID:        id,
+	// 		AppliedAt: appliedAt.String(),
+	// 	}
+
+	// 	records = append(records, record)
+
+	// 	// fmt.Fprintf(w, "%s %s\n", id, appliedAt)
+
+	// }
+
+	records, err := execMigrate(migrate.Down)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	bytes, _ := json.Marshal(records)
+
+	recordString := string(bytes)
+
+	fmt.Fprintf(w, "%s\n", recordString)
+
+	// fmt.Printf("Applied %d migrations!\n", n)
 }
